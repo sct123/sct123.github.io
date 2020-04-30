a7735948f83a55a081f87dfc2d4bcc9b6bdc373d
sct123 <3162103904@qq.com>
2020-05-01 03:39:22 +0800

    Site updated: 2020-05-01 03:39:21

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,1 +1,3 @@
+// build time:Fri May 01 2020 03:39:17 GMT+0800 (GMT+08:00)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
+//rebuild by neat 
---
+++ static/api/js/trans/data.js
@@ -0,0 +1,3 @@
+// build time:Fri May 01 2020 03:39:17 GMT+0800 (GMT+08:00)
+
+//rebuild by neat 
